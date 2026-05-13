02b7adfc1bd6f089e92777889fe5731ab784e20a
Leandro Da Souza <dasouza.leandro@gmail.com>
2026-05-13 12:35:06 +0200

    BASH-EX separera welcome delen från add user delen

--- add_user.sh
+++ add_user.sh
@@ -33,23 +33,4 @@ add_user() {
         # Se till att endast användaren har rättigheter
 	chmod -R 700 "/home/$username"
 	chown -R "$username:$username" "/home/$username"
-
-	# skapa välkomstfil och lägg till välkomsttext
-	local welcome_file="/home/$username/welcome.txt"
-
-        echo "Välkommen $username" > "$welcome_file"
-	echo "" >> "$welcome_file"
-	echo "Andra användare:" >> "$welcome_file"
-	echo "" >> "$welcome_file"
-
-        # loopa /etc/passwd efter användare och lägg till i välkomstfilen
-        while read line; do
-		# kolla användare i /home och att de inte är syslog
-		if [[ "$line" == *"/home"* && "$line" != syslog* && "$line" != "$username:"* ]]; then
-			# ta bara användarnamnet 
-			local username_from_file=$(cut -d: -f1 <<< "$line")
-			echo "$username_from_file" >> "$welcome_file"
-		fi
-	done < /etc/passwd
-
 }
--- create_users.sh
+++ create_users.sh
@@ -2,13 +2,20 @@
 
 # ta in guard.sh där vi kollar root och argument
 source ./guard.sh
-# ta in add_user.sh där vi skapar användare, mappar, filer
+# ta in add_user.sh där vi skapar användare och mappar
 source ./add_user.sh
+# ta in create_welcome_file där vi skapar välkomst
+source ./create_welcome_file.sh
 
 require_root
 require_args "$@"
 
 for arg in "$@"; do
 	add_user "$arg"
+done
+
+# separat loop för att se till att alla användare har skapat 
+for arg in "$@"; do
+	create_welcome_file "$arg"
 done
 
